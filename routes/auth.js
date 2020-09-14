ae8aa2f8b655f46e9b78734b0c17eff9289574cf
Jesse Quijano <jesse@quijano.net>
2020-09-13 23:28:31 -0700

    name route on export

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,11 @@ import express from 'express';
 import { userSignupValidator } from '../validator';
 import { signup, signin, signout, requireSignin } from '../controllers/auth';
 
-export const router = express.Router();
+const router = express.Router();
 
 // routes
 router.post('/signup', userSignupValidator, signup);
 router.post('/signin', signin);
 router.get('/signout', signout);
+
+export const authRoutes = router;
