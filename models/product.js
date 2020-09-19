cb0edb3a701e237f28fd939f6405ce8193aae8b9
Jesse Quijano <jesse@quijano.net>
2020-09-18 19:13:07 -0700

    add product model

---
+++ models/product.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+const { ObjectId } = mongoose.Schema; 
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        maxlength: 32
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 2000
+    },
+    price: {
+        type: Number,
+        trime: true,
+        required: true,
+        maxlength: 32
+    },
+    category: {
+        type: ObjectId,
+        ref: 'Category',
+        required: true
+    },
+    quantity: {
+        type: Number
+    },
+    photo: {
+        data: Buffer,
+        contentType: String
+    },
+    shipping: {
+        required: false,
+        type: Boolean
+    }
+}, { timestamps: true });
+
+export const Product = mongoose.model('Product', productSchema);
