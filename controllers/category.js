9be6285c3b5dd8b7dd2fc1fe92b3b838547655cc
Jesse Quijano <jesse@quijano.net>
2020-09-24 09:42:22 -0700

    add category route and controller

--- controllers/category.js
+++ controllers/category.js
@@ -1,6 +1,24 @@
 import { Category } from '../models/category';
 import { errorHandler } from '../helpers/dbErrorHandler';
 
+export const categoryById = (req, res, next, id) => {
+    Category.findById(id).exec((err, category) => {
+        if (err || !category) {
+            return res.status(400).json({
+                error: "Category not found"
+            });
+        }
+
+        req.category = category;
+
+        next();
+    });
+};
+
+export const read = (req, res) => {
+    return res.json(req.category);
+};
+
 export const create = (req, res) => {
     const category = new Category(req.body);
     
@@ -14,3 +32,47 @@ export const create = (req, res) => {
         res.json({data});
     });
 };
+
+export const update = (req, res) => {
+    const category = req.category;
+    category.name = req.body.name;
+
+    category.save((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+
+        res.json(data);
+    });
+};
+
+export const remove = (req, res) => {
+    const category = req.category;
+    
+    category.remove((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+
+        res.json({
+            message: 'Category deleted'
+        });
+    });
+};
+
+export const list = (req, res) => {
+    Category.find({}).sort({name: 'asc'}).exec((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+
+        res.json(data);
+    });
+};
+
--- routes/category.js
+++ routes/category.js
@@ -1,13 +1,18 @@
 import express from 'express';
-import { create } from '../controllers/category';
+import { categoryById, create, read, update, remove, list } from '../controllers/category';
 import { userById } from '../controllers/user';
 import { isAuth, isAdmin, requireSignin } from '../controllers/auth';
 
 const router = express.Router();
 
 // routes
+router.get('/category/:categoryId', read);
 router.post('/category/create/:userId', requireSignin, isAdmin, isAuth, create);
+router.put('/category/:categoryId/:userId', requireSignin, isAdmin, isAuth, update);
+router.delete('/category/:categoryId/:userId', requireSignin, isAdmin, isAuth, remove);
+router.get('/categories', list);
 
+router.param('categoryId', categoryById);
 router.param('userId', userById);
 
 export const categoryRoutes = router;
