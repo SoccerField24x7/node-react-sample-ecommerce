50dacab3455a1ff603ddabb3c78d94c357c66c1e
Jesse Quijano <jesse@quijano.net>
2020-11-11 10:11:59 -0800

    functions to set and unset session in local storage

--- react-app/src/auth/index.js
+++ react-app/src/auth/index.js
@@ -33,3 +33,25 @@ export const signin = (user) => {
         console.log(err);
     });
 };
+
+export const authenticate = (data, next) => {
+    if (typeof (window) !== 'undefined') {
+        localStorage.setItem('jwt', JSON.stringify(data));
+        next();
+    }
+};
+
+export const signout = (next) => {
+    if (typeof (window) !== 'undefined') {
+        localStorage.removeItem('jwt');
+        next();
+
+        return fetch(`${process.env.REACT_APP_API}/signout`, {
+            method: 'GET',
+        }).then((response) => {
+            console.log('signout', response);
+        }).catch((error) => {
+            console.log(error);
+        });
+    }
+};
