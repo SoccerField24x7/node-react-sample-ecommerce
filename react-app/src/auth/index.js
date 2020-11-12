e9dfc2b140ccb4733e1024acf9058cb4203c29ae
Jesse Quijano <jesse@quijano.net>
2020-11-12 09:15:24 -0800

    add isAuthenticated function

--- react-app/src/auth/index.js
+++ react-app/src/auth/index.js
@@ -41,6 +41,18 @@ export const authenticate = (data, next) => {
     }
 };
 
+export const isAuthenticated = () => {
+    if (typeof (window) == 'undefined') {
+        return false;
+    }
+
+    if (localStorage.getItem('jwt')) {
+        return JSON.parse(localStorage.getItem('jwt'));
+    }
+
+    return false;
+};
+
 export const signout = (next) => {
     if (typeof (window) !== 'undefined') {
         localStorage.removeItem('jwt');
