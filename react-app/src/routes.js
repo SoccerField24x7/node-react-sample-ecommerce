7eac62fce904fb9e8f8c61aeed8fc7fe9239694e
Jesse Quijano <jesse@quijano.net>
2020-11-13 07:50:14 -0800

    add protected routes

--- react-app/src/routes.js
+++ react-app/src/routes.js
@@ -3,7 +3,10 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Home from './components/core/Home';
 import Signup from './components/user/Signup';
 import Signin from './components/user/Signin';
-import Menu from './components/core/Menu';
+import AdminRoute from './auth/AdminRoute';
+import PrivateRoute from './auth/PrivateRoute';
+import Dashboard from './components/user/UserDashboard';
+import AdminDashboard from './components/user/AdminDashboard';
 
 const Routes = () => {
     return (
@@ -12,6 +15,8 @@ const Routes = () => {
                 <Route path="/" exact component={Home} />
                 <Route path="/signin" exact component={Signin} />
                 <Route path="/signup" exact component={Signup} />
+                <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
+                <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
             </Switch>
         </BrowserRouter>
     );
