ee89d573ba18591ec0bc5a91817bbc27b0af86bc
Jesse Quijano <jesse@quijano.net>
2020-11-23 17:36:27 -0800

    image display component

--- react-app/src/components/core/Image.js
+++ react-app/src/components/core/Image.js
@@ -1,1 +1,16 @@
 import React from 'react';
+
+const Image = ({item, url}) => {
+    return (
+        <div className="product-img">
+            <img 
+                className="mb-3"
+                alt={item.name}
+                style={{maxHeight: '100%', maxWidth: '100%'}}
+                src={`${process.env.REACT_APP_API}/${url}/photo/${item._id}`}
+            />
+        </div>
+    );
+};
+
+export default Image;
